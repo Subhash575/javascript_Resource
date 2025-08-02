bd42d64cb113d69b98c7f06f41723a7ba63ee471
Subhash <subhash100393@gmail.com>
2025-08-02 21:53:37 +0530

    learning dom

--- part8/script.js
+++ part8/script.js
@@ -17,3 +17,65 @@ document
     //We have access of paragraph here or we can say it hold the paragraph.
     p.textContent = "Paragraph Change";
   });
+
+//---------------------------------------------------------------------------------------
+
+// Example 2: Traversing the DOM
+
+document
+  .getElementById("higlightFirstCity")
+  .addEventListener("click", function () {
+    let citiesList = document.getElementById("citiesList");
+
+    //Learning:-
+
+    //To access the first child in the nested tag we use the `firstElementChild' property
+    // console.log(citiesList.firstElementChild);
+
+    //To list the tag on the tag we use this property here:
+    // console.log(citiesList.firstElementChild.classList);
+
+    citiesList.firstElementChild.classList.add("highlight");
+    //add is the method which take the `listName` here that `class `is `highlisght'
+  });
+//--------------------------------------------------------------------------------
+
+// Example 3: Manipulating DOM Element
+
+document.getElementById("changeOrder").addEventListener("click", function () {
+  //One way
+  // document.getElementById("coffeeType").textContent = "Espresso";
+
+  let coffeeType = document.getElementById("coffeeType");
+  coffeeType.textContent = "Espresso";
+
+  //We can also use `css-property' as well:-
+  coffeeType.style.backgroundColor = "Blue";
+  coffeeType.style.padding = "5px";
+});
+
+//----------------------------------------------------------------------------------------
+
+// Example 4: Creating and Inserting Elements
+let cnt = 1;
+document.getElementById("addNewItem").addEventListener("click", function () {
+  let shoppingList = document.getElementById("shoppingList");
+  let li = document.createElement("li");
+  li.textContent = `item${cnt++}`;
+  shoppingList.appendChild(li);
+});
+
+//-----------------------------------------------------------------------------------------
+
+// Example 5: Removing the DOM Element
+document
+  .getElementById("removeLastTask")
+  .addEventListener("click", function () {
+    let taskList = document.getElementById("taskList");
+
+    //This will remove the entire list
+    // taskList.remove();
+
+    //To remove the last child we need to use the following  property.
+    taskList.lastElementChild.remove();
+  });
