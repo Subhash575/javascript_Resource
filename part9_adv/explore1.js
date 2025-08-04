bf9caf70802519e2000d4ed8add8296b7d562a72
Subhash <subhash100393@gmail.com>
2025-08-04 22:12:52 +0530

    learn about asynchronous behaviour of Js

---
+++ part9_adv/explore1.js
@@ -0,0 +1,15 @@
+//Asynchronous Behaviour of Javascript.
+
+function sayHello() {
+  console.log("I would like to say Hello");
+}
+
+setTimeout(() => {
+  sayHello();
+}, 4000);
+
+console.log("coffee");
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
