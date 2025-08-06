d2127b0abd8bbc9a454cbbcc1613f74db70b2cf3
Subhash <subhash100393@gmail.com>
2025-08-06 23:21:03 +0530

    learn about closure in js

---
+++ part9_adv/closure.js
@@ -0,0 +1,22 @@
+//closure property we are going to learn here.
+/*
+(v.imp):-
+"Closure are basically function" the whole idea about this function is that they
+remember the environment in which they created.(This means that function can retain
+the variable which are declared outside of it.
+*/
+
+//Closure example:-
+
+function outerFunc() {
+  let counter = 4;
+  return function () {
+    counter++;
+    return counter;
+  };
+}
+
+let increment = outerFunc();
+console.log(increment()); // op: 5
+console.log(increment()); // op: 6
+console.log(increment()); // op: 7
